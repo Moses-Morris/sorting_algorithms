01a49d8dce0a1ffd359376288abade20b29a6f08
Moses-Morris <mosesmorrisdev@gmail.com>
2023-01-23 00:17:11 -0800

    Sort h and print array file.

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
- * bubble_sort - a function that sorts an array of integers in ascending order using the Bubble sort algorithm
+ * bubble_sort - sorts an array of integers in ascending order
  * @array: sort array.
  * @size: size of array.
  * Return: Void function.
@@ -14,19 +14,19 @@ void bubble_sort(int *array, size_t size)
 	size_t tmp;
 	size_t traverse;
 	size_t j;
-	
-	if(array == NULL || size < 2)
+
+	if (array == NULL || size < 2)
 	{
 		return;
 	}
-	
-	//Loop then move element to right position 
-	//Traverse
+	/*Loop then move element to right position */
+	/*Traverse*/
+
 	sizeOfArray = size - 1;
-	for(j = 0; j < sizeOfArray; j++)
+	for (j = 0; j < sizeOfArray; j++)
 	{
-		//check greater element presiding previous index
-		if(array[j] > array[j + 1])
+		/*check greater element presiding previous index*/
+		if (array[j] > array[j + 1])
 		{
 			tmp = array[j];
 			array[j] = array[j + 1];
